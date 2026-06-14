4b6db4c013ad249a8ac3a0855754f1e03a9bab08
karemaldawly2014-cmd <karem.aldawly2014@gmail.com>
2026-06-14 20:32:28 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Smart Power Mix Pro",
+    layout="wide"
+)
+
+st.title("⚡ Smart Power Mix Pro")
+st.success("Karim El Dawly Contracting")
+
+st.write("Welcome to Smart Power Management System 👷‍♂️⚡")
+
+st.info("Use the sidebar to navigate between modules.")
